40bcb052cdb6fe96d60aa8e55354b6ae6483653e
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-08-23 10:20:15 -0300

    logcat erro

--- app/src/main/java/com/clemente/primeiroappdio/MainActivity.kt
+++ app/src/main/java/com/clemente/primeiroappdio/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         soma()
+        throw Exception ("Meu primeiro erro")
     }
 
     fun soma(): Int {
